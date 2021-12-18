a786761b32b1d2a68abc5741d9bfbaca775c5840
XYisMe <juni5584@gmail.com>
2021-12-19 04:34:01 +0800

    Server and client base- hvnt done

--- src/project/chatbox/Client.java
+++ src/project/chatbox/Client.java
@@ -5,25 +5,135 @@
  */
 package project.chatbox;
 
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.net.Socket;
+
 /**
  *
  * @author hor_s
  */
-public class Client {
+public class Client extends Frame implements ActionListener, Runnable{
 
-    Client(String server, int port, String username, ClientGUI aThis) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    Socket skt;
+    Panel topCP, bottomCP;
+    List chatList;
+    TextField name, port, message;
+    Button connect, send, exit;
+    BufferedWriter bw;
+    BufferedReader br;
+    Thread thread;
+     
+     public Client (String string){
+          super(string);
+          topControlPanel();
+          bottomControlPanel();
+
+          setSize(600, 400);
+          setLocation(700, 100);
+          setBackground (Color.decode("#D8BE7E"));
+          setVisible(true);
+     }
+
+     public void topControlPanel(){
+          topCP = new Panel();
+          name = new TextField(20);
+          port = new TextField(5);
+          connect = new Button("Connect");
+          chatList = new List();
+
+          //add into Panel
+          topCP.add(name);
+          topCP.add(port);
+          topCP.add(connect);
+
+          //set location of elements
+          add(topCP, BorderLayout.NORTH);
+          add(chatList, BorderLayout.CENTER);
+
+          //CLICK LISTENER
+          connect.addActionListener(this);
+
+
+     }
+
+     public void bottomControlPanel(){
+          bottomCP = new Panel();
+          message = new TextField(20);
+          send = new Button("Send");
+          exit = new Button("Exit");
+
+          //add into panel
+          bottomCP.add(message);
+          bottomCP.add(send);
+          bottomCP.add(exit);
+
+          //set location of elements
+          add(bottomCP, BorderLayout.SOUTH);
+
+          //CLICK LISTENER
+          send.addActionListener(this);
+          exit.addActionListener(this);
+
+
+     }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource().equals(exit)){
+             System.exit(0);
+        }else if (e.getSource().equals(connect)){
+
+             try{
+                  skt = new Socket(name.getText(), Integer.parseInt(port.getText()));
+                  bw = new BufferedWriter (new OutputStreamWriter(skt.getOutputStream()));
+                  br = new BufferedReader (new InputStreamReader(skt.getInputStream()));
+                  name.setText(name.getText());
+
+                  thread = new Thread (this);
+                  thread.start();
+
+             }catch (IOException ioe){
+                  ioe.getMessage();
+             }
+        }else {
+             try{
+                  if(bw != null){
+                       bw.write(message.getText());
+                       bw.newLine();
+                       bw.flush();
+                       chatList.add("You: " + message.getText());
+                       message.setText("");
+                  }
+             }catch(IOException ioe){
+                       ioe.getMessage();
+             }
+        }
     }
 
-    boolean start() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    @Override
+    public void run() {
+        try{
+             skt.setSoTimeout(1000);
+        }catch(Exception e){
+             e.getMessage();
+        }
+
+        while (true){
+             try{
+                  String msg = br.readLine();
+                  if(msg == null){
+                       break;
+                  }
+                  chatList.add("Admin: " + msg);
+             }catch (Exception e){}
+        }
     }
 
-    void sendMessage(ChatMessage chatMessage) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-     public static void main(String[] args){
-         
+    public static void main(String[] args){
+         new Client("Client Chat Box");
      }
     
 }
--- src/project/chatbox/Server.java
+++ src/project/chatbox/Server.java
@@ -4,47 +4,137 @@
  */
 package project.chatbox;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
  *
  * @author tanxinyii
  */
-public class Server {
+public class Server extends Frame implements ActionListener, Runnable{
 
-    private int port;
-    private boolean keepGoing;
-
-    Server(int port, ServerGUI aThis) {
-        this.port = port;
-    }
-
-    private Server(int port) {
-        this(port,null);
-    }
-
-    public void start(){
-        keepGoing = true;
-        System.out.println("Connection Established!!");      
+    //declaration
+    Client client;
+    private final int port = 8080;
+    ServerSocket serverSocket;
+    Socket skt;
+    
+    Label status;
+    List chat;
+    Panel controlPanel;
+    TextField message;
+    Button send,exit;
+    BufferedReader br;
+    BufferedWriter bw;
+    
+    
+   
+    public Server(String msg) {
+        super(msg);
+        
+        controlPanel();
+        listen();
     }
     
-    public void stop(){
-        keepGoing = false;
-        System.out.println("Connection Closed!!");
-        try 
-        {
-            new Socket("localhost", port);
-	}
-            catch(Exception e) 
-        {
-            //stop
-	}
+    public void controlPanel(){
+        status = new Label("Status");
+        chat = new List();
+        message = new TextField(50);
+        controlPanel = new Panel();
+        send = new Button("Send");
+        exit = new Button("Exit");
         
+        //add control to the panel
+        controlPanel.setLayout(new FlowLayout());
+        controlPanel.add(message);
+        controlPanel.add(send);
+        controlPanel.add(exit);
+
+        //add to Frame
+        add(status, BorderLayout.NORTH);
+        add(chat, BorderLayout.CENTER);
+        add(controlPanel, BorderLayout.SOUTH);
+
+        //click listener
+        send.addActionListener(this);
+        exit.addActionListener(this);
+        
+        setSize(600, 400);
+        setLocation(50, 100);
+        setBackground (Color.decode("#E8B16D"));
+        setVisible(true);
     }
-     
+
+    public void listen(){
+        try{
+            serverSocket = new ServerSocket(port);
+            status.setText("Listening on Port: " + port);
+            skt = serverSocket.accept();
+            br = new BufferedReader(new InputStreamReader(skt.getInputStream()));
+            bw = new BufferedWriter(new OutputStreamWriter(skt.getOutputStream()));
+            bw.write("Hi! Welcome to Chat-RELAX!!");
+            bw.newLine();
+            bw.flush();
+            Thread th;
+            th = new Thread(this);
+            th.start();
+            
+        }catch (Exception e) {
+            status.setText(e.getMessage());
+        }
+    }
+    
+ 
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource().equals(exit)){
+            System.exit(0);
+        }else{
+            try{
+                //enter message
+                bw.write(message.getText());
+                bw.newLine();
+                bw.flush();
+
+                chat.add("Admin:    " + message.getText());
+                message.setText("");
+
+            }catch (IOException ioe){
+                status.setText(ioe.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        try{
+            skt.setSoTimeout(3000);
+        }catch (Exception e){}
+
+        status.setText("- Client Connected -");
+        while(true){
+            try{
+                String msgA = br.readLine();
+                if(msgA == null){
+                    serverSocket.close();
+                    break;
+                }
+                chat.add(client.name.getText()+": "+ msgA);
+            }catch(IOException ioe){}
+        }
+
+    }
+    
+        
+    // main class
     public static void main(String[] args){
         // TODO code application logic her
-        new ServerGUI().setVisible(true);
+        new Server("Server Program");
     }
     
 }
